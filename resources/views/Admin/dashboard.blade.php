426cd5818e2630cb0721d85ee49bed23ca756e26
ahmedessawy10 <elessawy238@gmail.com>
2023-04-11 17:45:22 +0200

    sd

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -73,7 +73,7 @@
             </div> <!-- end col-->
         </div> <!-- end row-->
 
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-xl-8">
                 <div class="card">
                     <div class="card-body">
@@ -230,7 +230,7 @@
                     </div> <!-- end card-body-->
                 </div> <!-- end card-->
             </div> <!-- end Col -->
-        </div> <!-- end row-->
+        </div> <!-- end row--> --}}
 
         <div class="row">
             <div class="col-xl-4">
@@ -280,7 +280,55 @@
                 </div> <!-- end card-->
             </div><!-- end col -->
 
+        <div class="row">
             <div class="col-xl-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="float-end">
+                            <div class="dropdown">
+                                <a class=" dropdown-toggle" href="#" id="dropdownMenuButton2"
+                                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="text-muted">All Members<i class="mdi mdi-chevron-down ms-1"></i></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton2">
+                                    <a class="dropdown-item" href="#">Locations</a>
+                                    <a class="dropdown-item" href="#">Revenue</a>
+                                    <a class="dropdown-item" href="#">Join Date</a>
+                                </div>
+                            </div>
+                        </div>
+                        <h4 class="card-title mb-4">Top Users</h4>
+
+                        <div data-simplebar style="max-height: 339px;">
+                            <div class="table-responsive">
+                                <table class="table table-borderless table-centered table-nowrap">
+                                    <tbody>
+
+                                        @foreach ( App\Models\User::where('type','!=','admin')->get()  as $user )
+                                            
+                                     
+                                        <tr>
+                                            <td style="width: 20px;"><img src="{{asset("Admin3/assets/images/users/".$user->profile_image)}}" class="avatar-xs rounded-circle " alt="..."></td>
+                                            <td>
+                                                <h6 class="font-size-15 mb-1 fw-normal">{{$user->name}}</h6>
+                                                <p class="text-muted font-size-13 mb-0"><i class="mdi mdi-map-marker"></i> {{$user->phone}}</p>
+                                            </td>
+                                            <td><span class="badge bg-soft-success font-size-12">Success</span></td>
+                                            <td class="text-muted fw-semibold text-end"><i class="icon-xs icon me-2 text-success" data-feather="trending-up"></i></td>
+                                        </tr>
+                                        @endforeach
+                                        <tr>
+                                            
+                                    </tbody>
+                                </table>
+                            </div> <!-- enbd table-responsive-->
+                        </div> <!-- data-sidebar-->
+                    </div><!-- end card-body-->
+                </div> <!-- end card-->
+            </div><!-- end col -->
+
+            {{-- <div class="col-xl-4">
                 <div class="card">
                     <div class="card-body">
                         <div class="float-end">
@@ -339,7 +387,7 @@
 
                     </div>
                 </div>
-            </div>
+            </div> --}}
 
             <div class="col-xl-4">
                 <div class="card">
