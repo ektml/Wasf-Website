ebc0f668385c4c0acb76f04861ab1607a8f52bb3
ahmedessawy10 <elessawy238@gmail.com>
2023-04-15 12:08:36 +0200

    ss

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -95,7 +95,7 @@ class CartController extends Controller
                         $total=$price-$descount;
                         }
                         
-                      $total=doubleval(number_format($total,2));
+                      $total=number_format($total,2);
                   return $this->returnData(201, 'there is discount' ,compact('total','discount'));
              }else{
                  $total=$price;
