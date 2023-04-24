a14db430cded6a71682335468053062747d1ea2e
ahmedessawy10 <elessawy238@gmail.com>
2023-04-24 16:36:09 +0200

    xcx

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -230,18 +230,19 @@ if($request->type=='public'){
 
     public function privateRequests()
     {
+        $filters=[];
         $filter=[];
        
         if(isset(request()->search)){
             // $requests = Requests::where('type', 'public')->where("user_id", auth()->user()->id)->orderBy('status')->get();  
             $filter=request()->search;
             $validSearchOptions = ['Pending', 'Completed','In Process','Finished'];
-            if(in_array('active',$filter)){
-                array_push($filter,'In Process');
-                array_push($filter,'Finished');
+            if(in_array('active',$filters)){
+                array_push($filters,'In Process');
+                array_push($filters,'Finished');
             }
           
-            $searchOptions = array_intersect($validSearchOptions , $filter);
+            $searchOptions = array_intersect($validSearchOptions , $filters);
             $requests = Requests::where('type', 'private')
                                 ->where("user_id", auth()->user()->id)
                                 ->when(count($searchOptions), function ($query) use ($searchOptions) {
@@ -254,14 +255,12 @@ if($request->type=='public'){
                     array_push( $searchOptions,'datedesending');
                 }
                
-                if(in_array('active',$filter)){
+                if(in_array('active',$filters)){
                     $aa=['In Process','Finished'];
-                    array_push( $searchOptions,'active');
-                    $searchOptions = array_diff($aa,$searchOptions);
+                    array_push( $filter,'active');
+                    $filter = array_diff($aa,$filter);
                 }
-                $filter=array();
-                $filter=$filter+$searchOptions;
-                array_push($filter,...$searchOptions);
+               
                 dd($filter);
                 $requests=$requests->paginate(20);
              
