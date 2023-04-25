98e2033c070316bf16378d187636376eba9e5b54
ahmedessawy10 <elessawy238@gmail.com>
2023-04-25 09:58:57 +0200

    ss

--- app/Http/Controllers/Api/FreelancerReservationController.php
+++ app/Http/Controllers/Api/FreelancerReservationController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Reservation;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Controllers\Api\ApiResponseTrait;
 
 
